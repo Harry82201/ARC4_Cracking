0bf00bfbe54d0a627dac87a2b526e3d3eadaedd2
Yizhou Shen <harry8220@outlook.com>
2020-11-13 23:10:13 -0800

    task1 except tb_syn finish

--- task1/task1.sv
+++ task1/task1.sv
@@ -4,9 +4,59 @@ module task1(input logic CLOCK_50, input logic [3:0] KEY, input logic [9:0] SW,
              output logic [9:0] LEDR);
 
     // your code here
+    logic [7:0] address_to_mem;
+	logic [7:0] data_to_mem;
+	logic [7:0] data_from_mem;
+	logic wren;
+	logic en;
+	logic rdy;
+	
+	task1_ctrl task1_ctrl_ins(.clk(CLOCK_50), .rst_n(KEY[3]), .rdy_init(rdy), .en_init(en));
 
-    s_mem s( /* connect ports */ );
+    s_mem s(.address(address_to_mem), .clock(CLOCK_50), .data(data_to_mem), .wren(wren), .q(data_from_mem));
 
     // your code here
+    init init_ins(.clk(CLOCK_50), .rst_n(KEY[3]), .en, .rdy, .addr(address_to_mem), .wrdata(data_to_mem), .wren);
 
 endmodule: task1
+
+module task1_ctrl(input logic clk, input logic rst_n, input logic rdy_init, output logic en_init);
+	
+	parameter Start = 3'b000;
+	parameter Wait = 3'b001;
+	parameter Start_init = 3'b010;
+	parameter Wait_init = 3'b011;
+	parameter Finish = 3'b100;
+	
+	logic [2:0] state, next_state;
+	
+	always_ff @(posedge clk or negedge rst_n) begin
+		if(rst_n == 0) state <= Start;
+		else state <= next_state;
+	end
+	
+	always_comb begin
+		case(state)
+			Start: 			next_state = Wait;				
+			Wait: 			if(rdy_init) next_state = Start_init;
+							else next_state = Wait;
+			Start_init: 	next_state = Wait_init;
+			Wait_init: 		if(rdy_init) next_state = Finish;
+							else next_state = Wait_init;
+			Finish: 		next_state = Finish;
+			default: 		next_state = 3'bxxx;
+		endcase
+	end
+	
+	always_comb begin
+		case(state)
+			Start: 			en_init = 0;
+			Wait: 			en_init = 0; 
+			Start_init: 	en_init = 1; 
+			Wait_init: 		en_init = 0; 
+			Finish: 		en_init = 0;  
+			default: 		en_init = 1'bx; 
+		endcase
+	end
+	
+endmodule
--- task1/tb_rtl_init.sv
+++ task1/tb_rtl_init.sv
@@ -1,5 +1,46 @@
+`timescale 1ps / 1ps
+
 module tb_rtl_init();
 
 // Your testbench goes here.
+    logic clk, rst_n, en, rdy, wren;
+	logic [7:0] addr, wrdata;
+	
+	init init_rtl(.clk, .rst_n, .en, .rdy, .addr, .wrdata, .wren);
+
+	initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+	 
+	initial begin
+		rst_n = 1;
+		#10;
+		rst_n = 0;
+		#10;
+		rst_n = 1;
+		#11000;
+		rst_n = 0;
+		#10;
+		rst_n = 1;
+		#1000;
+		rst_n = 0;
+		#10;
+		rst_n = 1;
+	end
+	 
+	initial begin
+		en = 1;
+		#40;
+		en = 0;
+		#5200;
+		en = 1;
+		#10;
+		en = 0;
+		#5200;
+		en = 1;
+		#10;
+		en = 0;
+	end
 
 endmodule: tb_rtl_init
